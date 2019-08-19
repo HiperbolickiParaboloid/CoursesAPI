78b7ba93268ac212acbbc9ca613aeead497c30ab
HiperbolickiParaboloid <mirko.mljnc@gmail.com>
2019-08-19 19:26:57 +0200

    Update

--- security.py
+++ security.py
@@ -24,6 +24,7 @@ def authenticate(username, password):
         print (i)
     username_mapping = {u.username: u for u in users}
     user = username_mapping.get(username, None)
+    users = []
     print(user)
     if user and safe_str_cmp(user.password, str(sha256(password.encode("utf-8")).hexdigest())): 
         return user
@@ -34,6 +35,7 @@ def identity(payload):
         user = User((len(users)+1), i.get("username",""), i.get("password",""))
         users.append(user)
     userid_mapping = {u.id: u for u in users}
+    users = []
     user_id = payload["identity"]
     print("user_id")
     return userid_mapping.get(user_id, None)
